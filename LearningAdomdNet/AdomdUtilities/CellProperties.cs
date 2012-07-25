e62c8b800a139cb57aabbd793db2e62036d3019a
Piotr Ludwiczuk <piotrosz@gmail.com>
2012-07-25 22:20:55 +0200

    Added utilities proj

---
+++ LearningAdomdNet/AdomdUtilities/CellProperties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdomdUtilities
+{
+	public enum CellProperties
+	{
+		ACTION_TYPE,
+		BACK_COLOR,
+		CELL_ORIDINAL,
+		FONT_FLAGS,
+		FONT_NAME,
+		FONT_SIZE,
+		FORE_COLOR,
+		FORMAT,
+		FORMAT_STRING,
+		FORMATTED_VALUE,
+		LANGUAGE,
+		UPDATABLE,
+		VALUE
+	}
+}
---
+++ LearningAdomdNet/AdomdUtilities/MdxStringExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdomdUtilities
+{
+	internal static class MdxStringExtensions
+	{
+		public static string Braces(this string value)
+		{
+			if (string.IsNullOrWhiteSpace(value))
+				throw new Exception("MDX element value cannot be null or white space");
+
+			if (value.Contains('[') || value.Contains(']'))
+				throw new Exception("MDX element already contains square braces");
+
+			return string.Format("[{0}]", value);
+		}
+
+		public static string JoinDots(this string[] values)
+		{
+			return string.Join(".", values);
+		}
+
+		public static string JoinComma(this string[] values)
+		{
+			return string.Join(",", values);
+		}
+
+		public static string Measures(this string value)
+		{
+			return new string[] { Braces("Measures"), Braces(value) }.JoinDots();
+		}
+
+		public static string Tuple(this string[] values)
+		{
+			return string.Format("({0})", values.JoinComma());
+		}
+
+		public static string Set(this string[] values)
+		{
+			return string.Format("{{{0}}}", values.JoinComma());
+		}
+	}
+}
