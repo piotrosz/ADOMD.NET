46ab9f7c2cc0da0ca4f50d9c9a9575e97c2da069
Piotr Ludwiczuk <piotrosz@gmail.com>
2012-09-04 00:13:20 +0200

    Added more MDX examples

---
+++ LearningAdomdNet/AdomdUtilities/QueryBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdomdUtilities
+{
+	public class QueryBuilder
+	{
+		public QueryBuilder Where(string expression)
+		{
+			// TODO
+			return this;
+		}
+
+		public QueryBuilder From(string cube)
+		{
+			// TODO
+			return this;
+		}
+
+		public QueryBuilder Select(string expression)
+		{
+			// TODO
+			return this;
+		}
+
+		public QueryBuilder Member(string expression)
+		{
+			// TODO
+			return this;
+		}
+
+		public QueryBuilder AxisProperties(string property)
+		{
+			// TODO
+			return this;
+		}
+	}
+}
